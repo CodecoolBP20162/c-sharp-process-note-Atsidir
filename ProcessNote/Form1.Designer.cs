23389b660919307304a037181ebe7a5b38d314d7
Atsidir <tamas.torjai@gmail.com>
2017-06-16 14:24:59 +0200

    final version

--- ProcessNote/Form1.Designer.cs
+++ ProcessNote/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.PID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.processname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CPU = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.memory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.runningtime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.starttime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PID,
+            this.processname,
+            this.CPU,
+            this.memory,
+            this.runningtime,
+            this.starttime});
+            this.listView1.ForeColor = System.Drawing.Color.Black;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 31);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(640, 379);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // PID
+            // 
+            this.PID.Text = "PID";
+            this.PID.Width = 50;
+            // 
+            // processname
+            // 
+            this.processname.Text = "Process Name";
+            this.processname.Width = 100;
+            // 
+            // CPU
+            // 
+            this.CPU.Text = "CPU usage";
+            this.CPU.Width = 100;
+            // 
+            // memory
+            // 
+            this.memory.Text = "Memory usage";
+            this.memory.Width = 100;
+            // 
+            // runningtime
+            // 
+            this.runningtime.Text = "Running Time";
+            // 
+            // starttime
+            // 
+            this.starttime.Text = "Start Time";
+            this.starttime.Width = 132;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(922, 422);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader PID;
+        private System.Windows.Forms.ColumnHeader processname;
+        private System.Windows.Forms.ColumnHeader CPU;
+        private System.Windows.Forms.ColumnHeader memory;
+        private System.Windows.Forms.ColumnHeader runningtime;
+        private System.Windows.Forms.ColumnHeader starttime;
     }
 }
 
--- ProcessNote/Form1.cs
+++ ProcessNote/Form1.cs
@@ -1,20 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+
+
+
 
 namespace ProcessNote
 {
     public partial class Form1 : Form
     {
+        static Timer t;
+       
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Timer();
+            t.Interval = 3000;
+            t.Tick += new EventHandler(showListGrid);
+            t.Start();
+
+
+        }
+
+        public void showListGrid(object sender, EventArgs e)
+        {
+            ListViewItem itm;
+            Process[] allProcceses = Process.GetProcesses();
+            string[] arr = new string[6]; ;
+            foreach (Process process in allProcceses)
+            {
+
+                if (process.Id != 0)
+                {
+                    PerformanceCounter ramCounter = new PerformanceCounter("Process", "Working Set", process.ProcessName);
+                    PerformanceCounter cpuCounter = new PerformanceCounter("Process", "% Processor Time", process.ProcessName);
+
+                    arr[0] = process.Id.ToString();
+                    arr[1] = process.ProcessName;
+                    arr[2] = cpuCounter.NextValue().ToString() + " %";
+                    arr[3] = (ramCounter.NextValue() / 1024 / 1024).ToString() + " MB";
+                    arr[4] = (DateTime.Now - process.StartTime).ToString();
+                    arr[5] = process.StartTime.ToString();
+
+                    itm = new ListViewItem(arr);
+
+                    listView1.Items.Add(itm);
+                }
+            }
+        }
+        
     }
+
 }
